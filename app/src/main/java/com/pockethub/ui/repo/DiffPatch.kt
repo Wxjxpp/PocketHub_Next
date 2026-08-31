e3d0a93724914ada6a8c4d489f5b3511b90dc13d
Wxjxpp <Wxjxpp@users.noreply.github.com>
2026-08-31 08:20:20 +0000

    fix: restore diff horizontal scroll state import

--- app/src/main/java/com/pockethub/ui/repo/DiffPatch.kt
+++ app/src/main/java/com/pockethub/ui/repo/DiffPatch.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.material.icons.Icons
